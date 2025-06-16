fd448ed7fe835b5606ee6c96e577471a9a0c24d8
Syahrul Ataufik <v-syahrul.ataufik@anteraja.id>
2025-06-16 20:57:23 +0700

    feat: fix lint route

--- src/app/api/images/[id]/route.ts
+++ src/app/api/images/[id]/route.ts
@@ -5,11 +5,10 @@ import path from "path";
 import fs from "fs/promises";
 import { IMAGE_MAP } from "@/lib/image-map";
 
-export async function GET(
-  _req: NextRequest,
-  { params }: { params: { id: string } },
-) {
-  const { id } = await params;
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // Or use regex/pathname parsing
+  if (!id) return NextResponse.json({ error: `Bad Request` }, { status: 400 });
   const filename = IMAGE_MAP[id];
   if (!filename) {
     return NextResponse.json({ error: "Image not found" }, { status: 404 });
