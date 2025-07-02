43f245e8a59cb772942eaeb0e12e8913285c2809
Syahrul Ataufik <v-syahrul.ataufik@anteraja.id>
2025-07-02 11:53:10 +0700

    feat: remove commend

--- src/app/guest/ist/[slug]/subtest/test/[type]/page.tsx
+++ src/app/guest/ist/[slug]/subtest/test/[type]/page.tsx
@@ -9,9 +9,6 @@ export default async function IstTestPage({ params }: PageType) {
     return notFound();
   }
 
-  // TODO: select ist session by istInvitationId and subtestTemplateId and startedAt == null
-  // if not found return notFound()
-  // when session already started, user can't open this page again.
   return (
     <IstWrapper>
       <IstSelectedTest slug={slug} type={type} />
